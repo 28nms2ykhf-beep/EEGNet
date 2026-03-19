0abd044e9e081b81efcf2849808fc0729318a386
vincent shi <28nms2ykhf@privaterelay.appleid.com>
2026-03-19 18:13:35 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import torch
 DATA_ROOT = "/home/sv25/Desktop/eeg_epochs_output"
 ANNOTATION_CSV = "/home/sv25/Desktop/ground_truth(Sheet1).csv"
 
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 SAMPLES_PER_SESSION = 1400
 NUM_ELECTRODES = 18
-CHUNK_SIZE = 1536
+CHUNK_SIZE = 2048
 NUM_CLASSES = 2
 
 F1 = 8
@@ -26,3 +26,4 @@ VAL_RATIO = 0.1
 SEED = 42
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
